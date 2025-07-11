545b73a6be079427953418869540c61dc6bd58ad
tuananh2005889 <tuannguyen.120910@gmail.com>
2025-07-11 17:56:46 +0700

    fix deploy

--- components/about/index.ts
+++ components/about/index.ts
@@ -1,10 +1,5 @@
 export { default as AboutHero } from './AboutHero';
-export { default as AboutStats } from './AboutStats';
 export { default as AboutContent } from './AboutContent';
 export { default as AboutCTA } from './AboutCTA';
-
-// Tab components
 export { default as AboutStory } from './tabs/AboutStory';
-export { default as AboutValues } from './tabs/AboutValues';
-export { default as AboutTeam } from './tabs/AboutTeam';
-export { default as AboutTimeline } from './tabs/AboutTimeline';
+
